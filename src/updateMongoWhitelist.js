88a42eaf5f1c580f021fed867598253035cf8f84
Ravid Goldenberg <ravid.g@mygoods.io>
2024-11-15 16:13:02 +0200

    Added depedncies and switched to using mongo SDK

--- src/updateMongoWhitelist.js
+++ src/updateMongoWhitelist.js
@@ -1,10 +1,27 @@
 const axios = require('axios');
-const crypto = require('crypto');
+const getClient = require('mongodb-atlas-api-client');
 
 // MongoDB Atlas Project Details
-var MONGODB_GROUP_ID = process.env.MONGODB_GROUP_ID;
-var MONGODB_PUBLIC_KEY = process.env.MONGODB_PUBLIC_KEY;
-var MONGODB_PRIVATE_KEY = process.env.MONGODB_PRIVATE_KEY;
+var MONGODB_GROUP_ID = '673064add3a71f000518adf8';
+var MONGODB_PUBLIC_KEY = 'hdfkalps';
+var MONGODB_PRIVATE_KEY = '2d23aa0b-2494-4575-badc-3a453013cc4a';
+
+// Initialize MongoDB Atlas Client
+const { projectWhitelist } = getClient({
+    "publicKey": MONGODB_PUBLIC_KEY,
+    "privateKey": MONGODB_PRIVATE_KEY,
+    "baseUrl": "https://cloud.mongodb.com/api/atlas/v1.0",
+    "projectId": MONGODB_GROUP_ID
+});
+
+const options = {
+    "envelope": true,
+    "itemsPerPage": 10,
+    "pretty": true,
+    "httpOptions": { // This parameter will not be sent as querystring. This will be send to http request package `urllib`
+        "timeout": 5000
+    }
+}
 
 // Function to Get Render's Public IP Address
 async function getPublicIP() {
@@ -12,34 +29,27 @@ async function getPublicIP() {
     return res.data.ip;
 }
 
-// Function to Create a MongoDB Atlas API Request
+// Function to Update the MongoDB Atlas Whitelist
 async function updateMongoDBWhitelist(ip) {
-    const date = new Date().toUTCString();
-    const path = `/api/atlas/v1.0/groups/${MONGODB_GROUP_ID}/accessList`;
+    try {
 
-    // Generate API request signature
-    const payload = `GET\n${date}\n${path}\n\n`;
-    const signature = crypto.createHmac('sha1', MONGODB_PRIVATE_KEY).update(payload).digest('base64');
-
-    // MongoDB API request headers
-    const headers = {
-        'Authorization': `MongoDB ${MONGODB_PUBLIC_KEY}:${signature}`,
-        'Date': date,
-        'Content-Type': 'application/json'
-    };
-
-    // Add the Render IP to the whitelist
-    await axios.post(`https://cloud.mongodb.com${path}`,
-        { ipAddress: ip, comment: 'Render Deployment' },
-        { headers }
-    );
+        let responseWhitelist = await projectWhitelist.create([{
+            ipAddress: ip,
+            comment: 'Render Deployment'
+        }])
+        console.log(responseWhitelist);
+        // let response = await projectWhitelist.getAll(options);
+        // console.log(response.content.results);
+        console.log('MongoDB IP whitelist updated with Render IP:', ip);
+    } catch (error) {
+        console.error('Error updating MongoDB whitelist:', error.response ? error.response.data : error.message);
+    }
 }
 
 (async () => {
     try {
         const renderIP = await getPublicIP();
         await updateMongoDBWhitelist(renderIP);
-        console.log('MongoDB IP whitelist updated with Render IP:', renderIP);
     } catch (error) {
         console.error('Error updating MongoDB whitelist:', error);
     }
